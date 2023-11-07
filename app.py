2f7eba5d3de54f74c19438cb933409a1c608a1bd
Keerthi-Korrapati <103488773+Keerthi-Korrapati@users.noreply.github.com>
2023-11-07 11:02:46 +0530

    Added microblog

---
+++ app.py
@@ -0,0 +1,43 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_app(): # Flask's app factory pattern
+    app = Flask(__name__) # Flask application Object , __name__ -> name of current python module or script
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.microblog # Database name represented to variable
+
+    entries = []
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        # print([e for e in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            entries.append((entry_content, formatted_date))
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({}) #
+        ]
+        return render_template("home.html", entries=entries_with_date)
+    return app
+
+
+
+
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
